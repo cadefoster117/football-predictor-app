407e4c625187eec34ae0ff3c1ac0fc753b427af1
cadefoster117 <48207680+cadefoster117@users.noreply.github.com>
2026-04-01 22:36:23 +0300

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process")
 console.log("Scheduler started")
 
 /* RUN EVERY DAY AT 00:00 */
-cron.schedule("20 10 * * *", () => {
+cron.schedule("0 0 * * *", () => {
 
  console.log("Daily scan started")
 
